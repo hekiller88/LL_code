95eec62c25ae5ed635f2d5af866407f374d2b21c
Liu He <lhe@emich.edu>
2018-11-25 10:09:04 -0500

    Create Lint_175.java

---
+++ DFS/Lint_175.java
@@ -0,0 +1,48 @@
+// https://www.lintcode.com/problem/invert-binary-tree/description
+
+// M1, traversal withou recursive
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void invertBinaryTree(TreeNode root) {
+        // write your code here
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curt = root;
+        
+        while (curt != null || !stack.empty()) {
+            while (curt != null) {
+                stack.push(curt);
+                curt = curt.left;
+            }
+            curt = stack.pop();
+            
+            TreeNode tmp = curt.left;
+            curt.left = curt.right;
+            curt.right = tmp;
+            
+            curt = curt.left;
+        }
+    }
+}
+
+// M2, recursive
+public class Solution {
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    public void invertBinaryTree(TreeNode root) {
+        // write your code here
+        if (root == null)
+            return;
+            
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        
+        invertBinaryTree(root.left);
+        invertBinaryTree(root.right);
+    }
+}
