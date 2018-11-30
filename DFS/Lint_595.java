cc74b8af1943c8c2b1a62a7a00eb4282fdf17ea4
Liu He <lhe@emich.edu>
2018-11-30 11:51:34 -0500

    Create Lint_595.java

---
+++ DFS/Lint_595.java
@@ -0,0 +1,63 @@
+// https://www.lintcode.com/problem/binary-tree-longest-consecutive-sequence/description?_from=ladder&&fromId=1
+
+// M1 top-down w/ global var
+public class Solution {
+    /**
+     * @param root: the root of binary tree
+     * @return: the length of the longest consecutive sequence path
+     */
+    int maxLen = 0;
+    public int longestConsecutive(TreeNode root) {
+        helper(root, null, 0);
+        
+        return maxLen;
+    }
+    
+    private void helper(TreeNode root, TreeNode parent, int len) {
+        if (root == null)
+            return;
+        
+        len = (parent != null && parent.val + 1 == root.val) ?
+              len + 1 :
+              1;
+        maxLen = Math.max(len, maxLen);
+        
+        helper(root.left, root, len);
+        helper(root.right, root, len);
+        
+    }
+    
+// M1 - 2: top-down without global var
+
+public int longestConsecutive(TreeNode root) {
+    return dfs(root, null, 0);
+}
+
+private int dfs(TreeNode p, TreeNode parent, int length) {
+    if (p == null) return length;
+    length = (parent != null && p.val == parent.val + 1) ? length + 1 : 1;
+    return Math.max(length, Math.max(dfs(p.left, p, length),
+                                     dfs(p.right, p, length)));
+}
+
+// M2, bottom-up
+private int maxLength = 0;
+public int longestConsecutive(TreeNode root) {
+    dfs(root);
+    return maxLength;
+}
+
+private int dfs(TreeNode p) {
+    if (p == null) return 0;
+    int L = dfs(p.left) + 1;
+    int R = dfs(p.right) + 1;
+    if (p.left != null && p.val + 1 != p.left.val) {
+        L = 1;
+    }
+    if (p.right != null && p.val + 1 != p.right.val) {
+        R = 1;
+    }
+    int length = Math.max(L, R);
+    maxLength = Math.max(maxLength, length);
+    return length;
+}
