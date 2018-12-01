0736651353178a7c0def59388f90500ccc6fdecc
Liu He <lhe@emich.edu>
2018-12-01 10:24:30 -0500

    Update Lint_901.java

--- DFS/Lint_901.java
+++ DFS/Lint_901.java
@@ -99,6 +99,79 @@ public class Solution {
     }
 }
 
+// M1 - 2, pred + succ, O(h)
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the given BST
+     * @param target: the given target
+     * @param k: the given k
+     * @return: k values in the BST that are closest to the target
+     */
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> ret = new ArrayList<>();
+        
+        TreeNode pred = pred(root, target);
+        TreeNode succ = succ(root, target);
+        for (int i = 0; i < k; i++) {
+            if (pred == null) {
+                ret.add(succ.val);
+                succ = succ(root, succ.val);
+            } else if (succ == null) {
+                ret.add(pred.val);
+                pred = pred(root, pred.val);
+            } else {
+                if (Math.abs(pred.val - target) < Math.abs(succ.val - target)) {
+                    ret.add(pred.val);
+                    pred = pred(root, pred.val);
+                } else {
+                    ret.add(succ.val);
+                    succ = succ(root, succ.val);
+                }
+            }
+        }
+        
+        return ret;
+    }
+    
+    private TreeNode pred(TreeNode root, double target) {
+        TreeNode pred = null;
+        while (root != null) {
+            if (root.val < target) {
+                pred = root;
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+        return pred;
+    }
+    
+    private TreeNode succ(TreeNode root, double target) {
+        TreeNode succ = null;
+        while (root != null) {
+            if (root.val > target) {
+                succ = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return succ;
+    }
+}
+
 // M2, optimal, maintain 2 stack
 // Stack predecessor: top is the largest node smaller than target
 // Stack successor  : top is the smallest node larger than target
