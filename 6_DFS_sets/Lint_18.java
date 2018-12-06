e05d9486b08f0c15780b3dda97a7ca115cbffd18
Liu He <lhe@emich.edu>
2018-12-05 22:18:02 -0500

    Update Lint_18.java

--- 6_DFS_sets/Lint_18.java
+++ 6_DFS_sets/Lint_18.java
@@ -37,19 +37,17 @@ public class Solution {
 
 // M2, Vague
 public List<List<Integer>> subsetsWithDup(int[] nums) {
-    Arrays.sort(nums);
-	List<List<Integer>> result = new ArrayList<List<Integer>>();
-	result.add(new ArrayList<Integer>());
-	int begin = 0;
-	for(int i = 0; i < nums.length; i++){
-		if(i == 0 || nums[i] != nums[i - 1]) begin = 0;
-		int size = result.size();
-		for(int j = begin; j < size; j++){
-			List<Integer> cur = new ArrayList<Integer>(result.get(j));
-			cur.add(nums[i]);
-			result.add(cur);
-		}
-		begin = size;
-	}
-	return result;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList();
+        ans.add(new ArrayList()); // add []
+        for (int i = 0, prev = 0; i < nums.length; i++) {
+            int size = ans.size();
+                for (int j = (i == 0 || nums[i] != nums[i - 1]) ? 0 : prev; j < size; j++) {
+                    List<Integer> cur = new ArrayList(ans.get(j));
+                    cur.add(nums[i]);
+                    ans.add(cur);
+                }
+            prev = size;
+        }
+        return ans;
 }
