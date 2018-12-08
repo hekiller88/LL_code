d11867913ba8fbc3cc6806b1400e33d9ad0d9104
Liu He <lhe@emich.edu>
2018-12-08 10:46:23 -0500

    Update Lint_18.java

--- 6 Conbination-based DFS/Lint_18.java
+++ 6 Conbination-based DFS/Lint_18.java
@@ -36,18 +36,34 @@ public class Solution {
 }
 
 // M2, add the number to the last pos
-public List<List<Integer>> subsetsWithDup(int[] nums) {
+public class Solution {
+    /**
+     * @param nums: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (nums == null)
+            return ret;
+            
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList();
-        ans.add(new ArrayList()); // add []
-        for (int i = 0, prev = 0; i < nums.length; i++) {
-            int size = ans.size();
-                for (int j = (i == 0 || nums[i] != nums[i - 1]) ? 0 : prev; j < size; j++) {
-                    List<Integer> cur = new ArrayList(ans.get(j));
-                    cur.add(nums[i]);
-                    ans.add(cur);
-                }
-            prev = size;
+        
+        ret.add(new ArrayList<>());
+        int start = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] != nums[i - 1])
+                start = 0;
+            
+            int size = ret.size();
+            for (int j = start; j < size; j++) {
+                List<Integer> newSubset = new ArrayList<>(ret.get(j));
+                newSubset.add(nums[i]);
+                ret.add(newSubset);
+            }
+            
+            start = size;
         }
-        return ans;
+        
+        return ret;
+    }
 }
