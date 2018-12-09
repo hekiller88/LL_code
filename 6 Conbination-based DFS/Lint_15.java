4831664f33b7c2a3322deb7e35397fe27dd8b6d4
Liu He <lhe@emich.edu>
2018-12-08 21:48:21 -0500

    Update Lint_15.java

--- 6 Conbination-based DFS/Lint_15.java
+++ 6 Conbination-based DFS/Lint_15.java
@@ -37,3 +37,35 @@ public class Solution {
         }
     }
 }
+
+// M2, iterative, insert num to any position
+public class Solution {
+    /*
+     * @param nums: A list of integers.
+     * @return: A list of permutations.
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<List<Integer>> ret = new LinkedList<>();
+        if (nums == null)
+            return ret;
+             
+        ret.add(new ArrayList<>());
+        
+        for (int num: nums) {
+            int size = ret.size();
+            for (; size > 0; size--) {
+                List<Integer> tmp = new ArrayList<>(ret.pollFirst());
+                
+                for (int i = 0; i <= tmp.size(); i++) {
+                    tmp.add(i,num);
+                    ret.add(new ArrayList<>(tmp));
+                    tmp.remove(i);
+                }
+            }
+        }
+        
+        return ret;
+    }
+    
+    
+}
