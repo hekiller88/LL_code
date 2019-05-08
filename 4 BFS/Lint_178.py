f46d90889b09313c0b2cb88e95075aa2879ed6ad
Liu He <lhe@emich.edu>
2019-05-07 22:55:49 -0400

    Update Lint_178.py

--- 4 BFS/Lint_178.py
+++ 4 BFS/Lint_178.py
@@ -29,3 +29,60 @@ class Solution:
                     q.append(neighbor)
                             
         return len(visited) == n
+
+ # Method 2.1 elegant, pythonic UnionFind
+class Solution:
+    """
+    @param n: An integer
+    @param edges: a list of undirected edges
+    @return: true if it's a valid tree, or false
+    """
+    def validTree(self, n, edges):
+        if len(edges) != n - 1:
+            return False
+            
+        parents = list(range(n)) # in python3
+            
+        def find(x):
+            return x if x == parents[x] else find(parents[x])
+            
+        def union(edge):
+            root_x, root_y = map(find, edge)
+            parents[root_x] = root_y
+            return root_x != root_y
+            
+        return all(map(union, edges))
+    
+    # Method2.2: optimal UnionFind, simple compression
+    class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        
+        self.parents = {i:i for i in range(n)}
+        self.size = n
+        
+        for u, v in edges:
+            self.union(u, v)
+            
+        return self.size == 1;
+        
+    def find(self, node):
+        path = []
+        while node != self.parents[node]:
+            path.append(node)
+            node = self.parents[node]
+            
+        for n in path:
+            self.parents[n] = node
+        
+        return node
+    
+    def union(self, u, v):
+        root_u, root_v = self.find(u), self.find(v)
+        if root_u != root_v:
+            self.size -= 1
+            self.parents[root_u] = root_v
+            
+            
+    
